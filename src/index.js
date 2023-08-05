cea997309d963d27a4c1a2fbd60f6b7cf932d1b4
AlexWeb2020 <m.mail2020@list.ru>
2023-08-05 12:43:08 +0300

    init: done task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 module.exports = function reverse (n) {
   
+  if (n < 0){n = n * (-1);}
+  let num = n.toString();
+  let numArr = num.split('').reverse().join('');
+  let revers = parseFloat(numArr);
+  
+  console.log(revers); 
+  return revers;
 }
+console.log(module.exports(-123));
